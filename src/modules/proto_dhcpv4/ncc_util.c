d5433da03384da428f09d06563aa34a89ea7a755
nchaigne <nchaigne@capgemini.fr>
2018-07-19 15:28:11 +0200

    Generic list handling functions

--- src/modules/proto_dhcpv4/ncc_util.c
+++ src/modules/proto_dhcpv4/ncc_util.c
@@ -37,6 +37,7 @@ int ncc_fr_event_timer_peek(fr_event_list_t *fr_el, struct timeval *when)
 	return 1;
 }
 
+
 /*
  *	Wrapper to fr_pair_find_by_da, which just returns NULL if we don't have the dictionary attr.
  */
@@ -73,6 +74,7 @@ VALUE_PAIR *ncc_pair_create_by_da(TALLOC_CTX *ctx, VALUE_PAIR **vps, fr_dict_att
 
 	return vp;
 }
+
 
 /*
  *	Resolve host address and port.
@@ -118,6 +120,7 @@ int ncc_host_addr_resolve(char *host_arg, ncc_endpoint_t *host_ep)
 	return 0;
 }
 
+
 /*
  *	Convert a struct timeval to float.
  */
@@ -141,4 +144,84 @@ int ncc_float_to_timeval(struct timeval *tv, float in)
 	tv->tv_sec = (time_t)in;
 	tv->tv_usec = (uint64_t)(in * USEC) - (tv->tv_sec * USEC);
 	return 0;
+}
+
+
+/*
+ *	Add an item entry to the tail of the list.
+ */
+void ncc_list_add(ncc_list_t *list, ncc_list_item_t *entry)
+{
+	if (!list || !entry) return;
+
+	if (!list->head) {
+		ncc_assert(list->tail == NULL);
+		list->head = entry;
+		entry->prev = NULL;
+	} else {
+		ncc_assert(list->tail != NULL);
+		ncc_assert(list->tail->next == NULL);
+		list->tail->next = entry;
+		entry->prev = list->tail;
+	}
+	list->tail = entry;
+	entry->next = NULL;
+	entry->list = list;
+	list->size ++;
+}
+
+/*
+ *	Remove an input entry from its list.
+ */
+ncc_list_item_t *ncc_list_item_draw(ncc_list_item_t *entry)
+{
+	if (!entry) return NULL; // should not happen.
+	if (!entry->list) return entry; // not in a list: just return the entry.
+
+	ncc_list_item_t *prev, *next;
+
+	prev = entry->prev;
+	next = entry->next;
+
+	ncc_list_t *list = entry->list;
+
+	ncc_assert(list->head != NULL); // entry belongs to a list, so the list can't be empty.
+	ncc_assert(list->tail != NULL); // same.
+
+	if (prev) {
+		ncc_assert(list->head != entry); // if entry has a prev, then entry can't be head.
+		prev->next = next;
+	}
+	else {
+		ncc_assert(list->head == entry); // if entry has no prev, then entry must be head.
+		list->head = next;
+	}
+
+	if (next) {
+		ncc_assert(list->tail != entry); // if entry has a next, then entry can't be tail.
+		next->prev = prev;
+	}
+	else {
+		ncc_assert(list->tail == entry); // if entry has no next, then entry must be tail.
+		list->tail = prev;
+	}
+
+	entry->list = NULL;
+	entry->prev = NULL;
+	entry->next = NULL;
+	list->size --;
+	return entry;
+}
+
+/*
+ *	Get the head input entry from a list.
+ */
+ncc_list_item_t *ncc_get_input_list_head(ncc_list_t *list)
+{
+	if (!list) return NULL;
+	if (!list->head || list->size == 0) { // list is empty.
+		return NULL;
+	}
+	// list is valid and has at least one element.
+	return ncc_list_item_draw(list->head);
 }
--- src/modules/proto_dhcpv4/ncc_util.h
+++ src/modules/proto_dhcpv4/ncc_util.h
@@ -5,11 +5,43 @@
 
 #include <freeradius-devel/server/base.h>
 
-/* Transport endpoint (IP address, port). */
+
+/*
+ *	Using rad_assert defined in include/rad_assert.h
+ */
+#define ncc_assert rad_assert
+
+
+typedef struct ncc_list_item ncc_list_item_t;
+
+/*
+ *	Chained list of data elements.
+ */
+typedef struct ncc_list {
+	ncc_list_item_t *head;
+	ncc_list_item_t *tail;
+	uint32_t size;
+} ncc_list_t;
+
+/*
+ *	Chained list item.
+ */
+struct ncc_list_item {
+	ncc_list_t *list;       //!< The list to which this entry belongs (NULL for an unchained entry).
+	ncc_list_item_t *prev;
+	ncc_list_item_t *next;
+
+	void *data;             //!< User-specific item data.
+};
+
+/*
+ *	Transport endpoint (IP address, port).
+ */
 typedef struct ncc_endpoint {
 	fr_ipaddr_t ipaddr;
 	uint16_t port;
 } ncc_endpoint_t;
+
 
 /* Get visibility on fr_event_timer_t opaque struct (fr_event_timer is defined in lib/util/event.c) */
 typedef struct ncc_fr_event_timer {
@@ -23,6 +55,7 @@ typedef struct ncc_fr_event_list {
 	/* We don't need anything beyond that. */
 } ncc_fr_event_list_t;
 
+
 int ncc_fr_event_timer_peek(fr_event_list_t *fr_el, struct timeval *when);
 
 VALUE_PAIR *ncc_pair_find_by_da(VALUE_PAIR *head, fr_dict_attr_t const *da);
@@ -34,3 +67,10 @@ int ncc_host_addr_resolve(char *host_arg, ncc_endpoint_t *host_ep);
 
 float ncc_timeval_to_float(struct timeval *in);
 int ncc_float_to_timeval(struct timeval *out, float in);
+
+void ncc_list_add(ncc_list_t *list, ncc_list_item_t *entry);
+ncc_list_item_t *ncc_list_item_draw(ncc_list_item_t *entry);
+ncc_list_item_t *ncc_get_input_list_head(ncc_list_t *list);
+
+#define NCC_LIST_ENQUEUE(_l, _e) ncc_list_add(_l, (ncc_list_item_t *)_e);
+#define NCC_LIST_DEQUEUE(_l) (void *)ncc_get_input_list_head(_l);
