b79b98bcc8205c7ac244788c5689f4feafc5c41a
nchaigne <nchaigne@capgemini.fr>
2019-03-29 16:46:59 +0100

    Need more FreeRADIUS libraries for xlat

--- src/modules/proto_dhcpv4/dhcperfcli.mk
+++ src/modules/proto_dhcpv4/dhcperfcli.mk
@@ -4,6 +4,9 @@ SOURCES		:= dhcperfcli.c ncc_util.c dpc_packet_list.c dpc_util.c
 # Using FreeRADIUS libraries:
 # - libfreeradius-util
 # - libfreeradius-dhcpv4 (fr_dhcpv4_*)
+# - libfreeradius-unlang (for xlat engine)
 
 TGT_PREREQS	:= libfreeradius-util.a libfreeradius-dhcpv4.a
+TGT_PREREQS	+= libfreeradius-unlang.a libfreeradius-server.a
+
 TGT_LDLIBS	:= $(LIBS)
