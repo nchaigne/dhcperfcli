7a401a51bb116c912b09cd0212570b94fffa2637
nchaigne <nchaigne@capgemini.fr>
2019-09-23 11:20:13 +0200

    Source file doesn't exist anymore

--- src/modules/proto_dhcpv4/dhcperfcli.mk
+++ src/modules/proto_dhcpv4/dhcperfcli.mk
@@ -2,7 +2,7 @@ TARGET		:= dhcperfcli
 SOURCES		:= dhcperfcli.c
 SOURCES		+= ncc_util.c
 SOURCES		+= ncc_util_server.c ncc_xlat_core.c ncc_xlat_func.c
-SOURCES		+= dpc_config.c dpc_packet_list.c dpc_util.c dpc_xlat.c
+SOURCES		+= dpc_config.c dpc_packet_list.c dpc_util.c
 SOURCES		+= dpc_segment.c
 
 # Using FreeRADIUS libraries:
